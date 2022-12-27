426745e8b5ae2489028d6ec691b2b6e16f7e43c8
remyla2003 <121510696+remyla2003@users.noreply.github.com>
2022-12-27 14:04:22 -0300

    Add files via upload

---
+++ questao2.m
@@ -0,0 +1,16 @@
+%REMYLA MARIANO DE MENDONÇA
+%20223701250
+%DATA: 23/12/2022
+%QUESTÃO 2
+
+n1= input("Digite o primeiro numero:");
+n2= input("Digite o segundo numero:");
+n3= input("Digite o terceiro numero:");
+
+if(n1>n2 && n1>n3)
+  disp("O maior valor é n1= ",n1);
+elseif(n2>n3 && n1>n3)
+  disp("O maior valor é n2= ",n2);
+else(n3>n1 & n3>n2)
+  disp ("O maior valor é n3= ",n3);
+end
